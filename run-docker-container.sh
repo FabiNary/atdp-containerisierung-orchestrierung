1647a68c9b92010129166b754488ac40b5947b68
FStefer <fabian.stefer@obi.de>
2024-05-05 16:59:36 +0200

    initial commit
    remove solutions

--- run-docker-container.sh
+++ run-docker-container.sh
@@ -1,4 +1,2 @@
 docker network create app-network
 docker run --name example-mysql --network=app-network -e MYSQL_ROOT_PASSWORD=mysql-pw -e MYSQL_DATABASE=node_example -p 3306:3306 -d mysql:latest
-docker build -t example-docker-service .
-docker run -p 3000:3000 --network=app-network example-docker-service
